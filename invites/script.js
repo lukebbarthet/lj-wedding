3a71745bda1575e0f6566e73745224f4d4bd85cd
lukeborgbarthet <luke@bb2000.com.mt>
2026-03-05 19:11:10 +0100

    animation speed 2

--- invites/script.js
+++ invites/script.js
@@ -3,7 +3,7 @@
 window.addEventListener("load", () => {
   setTimeout(() => {
     document.getElementById("invite-loader").classList.add("hide");
-  }, 600); // 1 seconds forced animation
+  }, 1500); // 1.5 seconds forced animation
 });
 
 const card = document.getElementById("card");
@@ -18,11 +18,11 @@ function flipCard() {
 
 card.addEventListener("click", flipCard);
 
-// Auto flip after 7 seconds if untouched
+// Auto flip after 3 seconds if untouched
 setTimeout(() => {
   if (!userInteracted) {
     card.classList.add("is-flipped");
     websiteBtn.classList.add("show");
   }
-}, 7000);
+}, 3000);
 
